3088b1f013d778470b1081c8411ff290dc9e919d
Alex Wiederkehr <asw360@gmail.com>
2024-06-26 14:52:15 -0500

    reverted link change

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "fiwest.com",
+  site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap()],
 });
